17b591da908395f95d41fbf8102c98d17e7b1170
DovletAtakishiyev <akamato1329@gmail.com>
2021-10-08 15:16:16 +0500

    est sayChert()

--- JavTest.java
+++ JavTest.java
@@ -3,4 +3,8 @@ public class JavTest {
     public static void main(String[] args) {
         System.out.println("Yo niggery!");
     }
+
+    public static void sayChert() {
+        System.out.println("Ty chert!");
+    }
 }
